a42536b815186a8817594820d58878dbb0b04f9c
daniel cooder <danielcooder@gmail.com>
2024-07-22 17:18:57 -0300

    feat: tranformando integer para string

---
+++ src/main/java/testetipos/TesteTipos.java
@@ -0,0 +1,17 @@
+package testetipos;
+
+public class TesteTipos {
+
+
+    public static void main(String[] args) {
+
+        int idade = 30;
+
+        String valor = Integer.toString(idade);
+        System.out.println(valor);
+
+    }
+
+
+
+}
